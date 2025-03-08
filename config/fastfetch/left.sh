3f6583299651e391fdb4c584ef24efd8beba58ae
Warthunder <88217087+warthunder1969@users.noreply.github.com>
2025-03-08 17:04:26 -0500

    Updated Fastfetch to current
    based on fastfetch from Ddubs

---
+++ config/fastfetch/left.sh
@@ -0,0 +1,32 @@
+# See this wiki page for more info:
+# https://github.com/dylanaraps/neofetch/wiki/Customizing-Info
+# Challenge script by TechnoDaft#0647
+
+# All the Timing information
+let Minute=60
+let Hour=3600
+let Day=86400
+let Week=604800
+
+# Year_days=365.25 days # 4 years (1461 days cause leap year) Divided by 4
+# Month_days=30.4375 days # Year Divided by 12
+let Month=2629800
+let Year=31557600
+
+# Year = 365 days, Month = 30 days.
+# let Month=2592000
+# let Year=31536000
+
+# Calculation of everything needed
+let current=$(date +%s)
+let birth_install=$(stat -c %W /) # Comment out if using the custom Epoch
+let challenge=(Day *730)
+let challenge_length=(challenge / Day)
+let challenge_complete=(birth_install + challenge)
+let diff_left=(challenge_complete - current)
+let diff_done=(current - birth_install)
+
+let Days_done=(diff_done / Day)
+let Days_left=(diff_left / Day)
+
+echo $Days_left days left
---
+++ config/fastfetch/today.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# See this wiki page for more info:
+# https://github.com/dylanaraps/neofetch/wiki/Customizing-Info
+# Challenge script by TechnoDaft#0647
+
+# All the Timing information
+let Minute=60
+let Hour=3600
+let Day=86400
+let Week=604800
+
+# Year_days=365.25 days # 4 years (1461 days cause leap year) Divided by 4
+# Month_days=30.4375 days # Year Divided by 12
+let Month=2629800
+let Year=31557600
+
+# Year = 365 days, Month = 30 days.
+# let Month=2592000
+# let Year=31536000
+
+# Calculation of everything needed
+let current=$(date +%s)
+echo $current | awk '{print strftime("%m/%d/%Y",$1)}'
