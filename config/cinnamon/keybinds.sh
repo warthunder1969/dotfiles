c95701caf0c148ba96a98295e9fdafb148bdafe1
Warthunder <88217087+warthunder1969@users.noreply.github.com>
2025-04-28 12:08:58 -0400

    Delete config/cinnamon directory

--- config/cinnamon/keybinds.sh
+++
@@ -1,8 +0,0 @@
-#source: https://forums.linuxmint.com/viewtopic.php?t=315472
-#To backup to a text file called ~/mykeybindings
-
-dconf dump /org/cinnamon/desktop/keybindings/ > mykeybindings
-
-#To reload on another machine running Cinnamon transfer the file to ~/mykeybindings on the #other machine and run
-
-dconf load /org/cinnamon/desktop/keybindings/ < mykeybindings
