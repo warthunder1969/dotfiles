7ec8d1c657b87faa345da6049f4151a61ce1d799
Warthunder <88217087+warthunder1969@users.noreply.github.com>
2025-05-07 09:28:46 -0400

    Update install.sh

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -37,14 +37,15 @@ git clone $dotfiles
 
 # Pulling in dots to the right directories
 
-cp -r $HOME/dotfiles/config/{gtk-3.0,fastfetch} $config
+cp -r $HOME/dotfiles/config/gtk-3.0 $config
+cp -r $HOME/dotfiles/config/fastfetch $config
 cp -r $HOME/dotfiles/config/bashrc $HOME/.bashrc $HOME
 cp -r $HOME/dotfiles/scripts/tools/*.sh $scripts
-cp -r $HOME/dotfiles/themes/icons/*.png $icons
+cp -r $HOME/dotfiles/themes/logos/*.png $icons
 #cp -r $HOME/dotfiles/themes/Mintyz.zip $themes
 #dpkg -i $HOME/dotfiles/themes/Mint/*.deb
 
-dconf load /org/cinnamon/desktop/keybindings/ < $dotfiles/config/cinnamon/keybinds
+dconf load /org/cinnamon/desktop/keybindings/ < $HOME/dotfiles/config/cinnamon/keybinds
 dconf load /org/cinnamon/ < $HOME/dotfiles/config/cinnamon/cinnamon
 #figure out how to do applets, probably some wget nonsense
 
