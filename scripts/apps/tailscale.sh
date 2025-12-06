af7a65198774fc3ca1c7e1894f3372c27e51a9eb
Warthunder <warthundersnerdcave@gmail.com>
2025-12-06 08:48:04 -0500

    Updated tailscale script

--- scripts/apps/tailscale.sh
+++ scripts/apps/tailscale.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Warthunder's Tailscale Installation Script
-#Version 1.2
+#Version 1.3
 
 #Variables
 dependencies="curl"
@@ -8,13 +8,5 @@ package="tailscale-systray"
 #Tailscale Linux Install
 #https://tailscale.com/download
 curl -fsSL https://tailscale.com/install.sh | sh
-
-#Tray Applet
-#Source: https://github.com/mattn/tailscale-systray
-#Easy Installer: https://github.com/C10udburst/tailscale-systray
-
-curl -fsSL https://raw.githubusercontent.com/C10udburst/tailscale-systray/master/install.txt | sh
-
-cp ~/.config/autostart/tailscale-systray.desktop ~/.local/share/applications/
-
+sudo tailscale set --operator=$USER
 exit 
