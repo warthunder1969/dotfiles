5971efe54d2ff515b7409620620f94502b4b5415
Warthunder <warthunder4820@protonmail.com>
2026-01-19 18:57:07 -0500

    Refactor and Release of Install 3.0

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,106 +1,156 @@
-#bin/bash
+#!/bin/bash
 # Installer script for Linux Mint. Should work for most Debian or Ubuntu based systems
 #Inspired by TheLinuxCast openSuse Install script
-# Version 2.0
+# Version 3.0
+
+# Basic Setup & Root Check ---
+set -e # Exit immediately if a command fails
+# Ensure the script is run as root
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root (use sudo)"
+  exit
+fi
 
 # Dependencies
-# git
-# wget
-# curl
-echo "Installing Depenencies"
-dpkg -l | grep -qw git  || sudo apt install -yyq git
-dpkg -l | grep -qw wget || sudo apt install -yyq wget
-dpkg -l | grep -qw curl || sudo apt install -yyq curl
+# Ensure whiptail is installed
+if ! command -v whiptail >/dev/null 2>&1; then
+    sudo apt update && sudo apt install -y whiptail
+fi
 
-sudo apt update
-sudo apt upgrade -y
-
-## Variables
+# Variables
 config="$HOME/.config"
 dotfiles="https://codeberg.org/warthunder1969/dotfiles.git"
-packages="./packages.txt"
-flatpaks="./flatpaks.txt"
-scripts="$HOME/.local/share/nemo/scripts"
-icons="$HOME/.icons"
-themes="$HOME/.theme"
 
-# Make some Directories
+# Define Package Lists
+## CORE PROFILE
+apt_core=("curl" "git" "wget" "micro" "htop" "btop" "nvtop" "s-tui" "duf" "eza" "nala" "cmatrix" "cpufetch" "ncdu" "speedtest-cli" "guvcview" "dconf-editor" "nextcloud-desktop" "keepassxc" "vlc" "adwaita-qt")
+flat_core=("com.vivaldi.Vivaldi" "com.google.Chrome" "com.github.tchx84.Flatseal" "dev.vencord.Vesktop" "im.riot.Riot" "com.notesnook.Notesnook" "org.localsend.localsend_app")
 
-mkdir -p $icons
-mkdir -p $themes
+## GAMING PROFILE
+apt_gaming=("steam" "libvulkan1" "mesa-vulkan-drivers")
+flat_gaming=("net.lutris.Lutris" "com.heroicgameslauncher.hgl" "net.davidotek.pupgui2")
 
-cd $HOME
-# Go Get Dotfiles
-git clone $dotfiles
-
-# Repository Packages
-xargs sudo apt-get -y install < $packages
-
-# Deb Packages
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.44.0/fastfetch-linux-amd64.deb #This won't be needed after LMDE 7 and Mint 23
-sudo dpkg -i *.deb
-sudo rm -rf *.deb
+## CODING PROFILE
+apt_coding=("build-essential" "python3-pip" "gcc" "cmake")
+flat_coding=("com.vscodium.codium")
 
 
-#Compiled Apps
-#cd $HOME/Downloads
 
-#cd $HOME
+# Function for the Main Menu
+show_main_menu() {
+    whiptail --title "Linux Mint Post-Install Tool" --menu "Choose an action:" 15 60 5 \
+    "1" "Update & Upgrade System" \
+    "2" "Install Software Suites" \
+    "3" "Configure System Settings" \
+    "4" "Cleanup & Optimize" \
+    "5" "Exit" 3>&1 1>&2 2>&3
+}
 
-## Flatpak Packages
-xargs flatpak install -y < $flatpaks
+# Logic Loop
+while true; do
+    CHOICE=$(show_main_menu)
 
-#set flaptak gloal permissions
-sudo flatpak override --device=dri
-sudo flatpak override --filesystem=home
+    case $CHOICE in
+        1)
+            echo "Updating system..."
+            sudo apt update && sudo apt upgrade -y && flatpak update -y && cinnamon-spice-updater --update-all && sudo mintupdate-cli refresh-cache
+            read -p "Press enter to continue..."
+            ;;
+        2)
+            clear
+            CHOICES=$(whiptail --title "System Persona Selection" --checklist \
+            "Select the roles for this machine (Space to toggle):" 15 60 3 \
+            "CORE" "Essential Apps, CLI tools & Flatseal" ON \
+            "GAMING" "Steam + Lutris/Heroic" OFF \
+            "CODING" "Compilers + Coding Tools" OFF \
+            3>&1 1>&2 2>&3)
 
-## Snap Packages
+            # Exit if Cancel is pressed
+            [ $? -ne 0 ] && exit 1
+           # Logic Gates
+            FINAL_APT=()
+            FINAL_FLAT=()
 
-## Appimages
+            if [[ $CHOICES =~ "CORE" ]]; then
+                FINAL_APT+=("${apt_core[@]}")
+                FINAL_FLAT+=("${flat_core[@]}")
+            fi
 
-## Loading Dotfiles
-# Pulling in dots to the right directories
+            if [[ $CHOICES =~ "GAMING" ]]; then
+                FINAL_APT+=("${apt_gaming[@]}")
+                FINAL_FLAT+=("${flat_gaming[@]}")
+            fi
 
-#cp -r $HOME/dotfiles/config/gtk-3.0/bookmarks $config/gtk-3.0
-#cp -r $HOME/dotfiles/config/fastfetch $config
-#cp -r $HOME/dotfiles/config/bashrc $HOME/.bashrc
-#cp -r $HOME/dotfiles/config/starship.toml $config
-#cp -r $HOME/dotfiles/scripts/tools/*.sh $scripts
-cp -r $HOME/dotfiles/themes/icons/lm-logo-*.png $icons
+            if [[ $CHOICES =~ "CODING" ]]; then
+                FINAL_APT+=("${apt_coding[@]}")
+                FINAL_FLAT+=("${flat_coding[@]}")
+            fi
+            #Execute
+            # Install APT Packages
+            if [ ${#FINAL_APT[@]} -gt 0 ]; then
+                echo "Installing System Packages..."
+                sudo apt update
+                sudo apt install -y "${FINAL_APT[@]}"
+            fi
 
-# Symlinks
-#In the Event you prefer symlinks 
-#ln -sf <target_path> <link_path>
+            # Install Flatpaks
+            if [ ${#FINAL_FLAT[@]} -gt 0 ]; then
+            echo "Installing Flatpaks..."
+            # Ensure Flathub is enabled
+            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            flatpak install flathub "${FINAL_FLAT[@]}" -y
+            fi
 
-#Desktop Shortcuts
-ln -s $HOME/Documents $HOME/Desktop/Documents
-ln -s $HOME/Pictures $HOME/Desktop/Pictures
-ln -s $HOME/Videos $HOME/Desktop/Videos
+            echo "Done!"
+            sleep 2
+            ;;
+        3)
+        echo "Loading System Configuration..."
+        # Set Reasonable Flaptak gloal permissions
+        sudo flatpak override --device=dri
+        sudo flatpak override --filesystem=home
+        flatpak override --user --filesystem=xdg-config/gtk-4.0
+        sudo flatpak override --filesystem=xdg-config/gtk-4.0
 
-# Dotfiles
-#ln -sf $HOME/Nextcloud/Projects/Linux/dotfiles $HOME #Uncommend for Systems that run Nextcloud & sync dotfiles locally
-ln -sf $HOME/dotfiles/config/gtk-3.0/bookmarks $config/gtk-3.0/bookmarks
-ln -sf $HOME/dotfiles/config/fastfetch $config
-ln -sf $HOME/dotfiles/config/bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/config/starship.toml $config/starship.toml
-ln -sf $HOME/dotfiles/scripts/tools $scripts
+        #Aquire dotfiles
+        cd $HOME
+        git clone $dotfiles
 
-# Load in settings through dconf
-dconf load /com/gexperts/Tilix/ < $HOME/dotfiles/config/tilix.dconf
-dconf load /org/cinnamon/ < $HOME/dotfiles/config/cinnamon/cinnamon
-dconf load /org/cinnamon/desktop/keybindings/ < $HOME/dotfiles/config/cinnamon/keybindings
-# Restart Cinnamon Shell
-cinnamon --replace >/dev/null 2>&1 &
-## Themes
-gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
-gsettings set org.cinnamon.theme name "Mint-Y-Dark"
+        #Import Core Settings & Keybindings:
+        dconf load /org/cinnamon/ < $HOME/dotfiles/config/cinnamon/cinnamon.dconf
+        dconf load /org/cinnamon/desktop/keybindings/ < $HOME/dotfiles/config/cinnamon/keybindings.dconf
+		
+        ## Themes
+        gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y"
+        gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
+        gsettings set org.cinnamon.theme name "Mint-Y-Dark"
+        cp -r $HOME/dotfiles/themes/icons/lm-logo-*.png $HOME/.icons
 
-#MS Fonts
-sudo apt install -yy ttf-mscorefonts-installer
-#Nerd Fonts
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
-&& cd ~/.local/share/fonts \
-&& unzip JetBrainsMono.zip \
-&& rm JetBrainsMono.zip \
-&& fc-cache -fv
+        # MS Fonts
+        sudo apt install -yy ttf-mscorefonts-installer
+        #Nerd Fonts
+        wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
+        && cd ~/.local/share/fonts \
+        && unzip JetBrainsMono.zip \
+        && rm JetBrainsMono.zip \
+        && fc-cache -fv
+            echo "Done!"
+            sleep 2
+            ;;
+        4)
+            echo "Cleaning up..."
+            sudo apt autoremove -y && sudo apt clean
+            flatpak uninstall --unused --delete-data
+            flatpak repair
+            read -p "Press enter to continue..."
+            ;;
+        5|"") # Exit if 5 is chosen or if the user hits 'Cancel'
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option"
+            ;;
+    esac
+done
+
